127000bd25ad76c711ca0183e9d592342adb871b
Ethan Madden <ethan.madden@my.simpson.edu>
2021-09-02 15:33:58 -0500

    Lab One

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
-print("Lab one")
-print("Has been coolio")
+print("Lab one has been cool.")
+print("It has been fun.")
 print("Hopefully this \"runs\"")
-print("We'll see lol")
+print("We'll see how it goes.")
