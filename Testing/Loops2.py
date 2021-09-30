99f553ee86cb4f0eda2a4f53259502cdb59179be
Ethan Madden <ethan.madden@my.simpson.edu>
2021-09-30 14:18:17 -0500

    Lab 4

--- Testing/Loops2.py
+++ Testing/Loops2.py
@@ -1,4 +1,19 @@
-i = 10
-while i > -1:
-    print(i)
-    i -= 1
+def count_up(start, end):
+    for number in range(start, end + 1):
+        print(number)
+
+count_up(5, 10)
+
+done = False
+
+while not done:
+    quit = input("Do you want to quit?")
+    if quit.lower()== "y":
+        done = True
+        print("Bye!")
+        break
+
+    attack = input("Do you want to attack the dragon?")
+    if attack.lower() == "y":
+        done = True
+        print("Bad choice")
--- Testing/test.py
+++ Testing/test.py
@@ -1,5 +1,36 @@
-print(""" 
-Something
-something
-lines
-""")
+while True: # Loop forever
+    quit = input("Do you want to quit? ")
+    if quit == "y":
+        break
+
+    attack = input("Does your elf attack the dragon? ")
+    if attack == "y":
+        print("Bad choice, you died.")
+        break
+
+    attack = input("Does your elf attempt to steal the gold? ")
+    if attack == "y":
+        print("Bad choice, you died.")
+        break
+
+done = True
+    while done:
+        sick = input("Would you like to un-alive?")
+        if sick == "y" or "yes":
+            done = False
+
+    done = False
+    while not done:
+        run = input("Nice. Lets go")
+        if run == "y" or "yes":
+            print("The border is" 'total' "miles away. Do you want to:")
+            print("a. Sprint")
+            print("b. Hide")
+            print("c. Check health")
+            print("d. Walk without concern")
+            done = False
+        sprint = input("You have made it" "miles")
+        if sprint == "a":
+            print("You made it")
+            done = False
+            continue
