c068a646834b15dd60faaecba039db5c9bd1bbfc
Ethan Madden <ethan.madden@my.simpson.edu>
2021-12-02 16:23:50 -0600

    Final Lab Part 1

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -11,8 +11,8 @@ SPRITE_SCALING = 0.5
 SPRITE_SCALING_PLAYER = 0.5
 SPRITE_SCALING_HEALTH = 1
 SPRITE_SCALING_NOT_HEALTH = 1
-HEALTH_COUNT = 80
-ENEMY_COUNT = 8
+HEALTH_COUNT = 2
+ENEMY_COUNT = 15
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -27,6 +27,48 @@ CAMERA_SPEED = 0.1
 
 # How fast the character moves
 PLAYER_MOVEMENT_SPEED = 7
+
+class Player(arcade.Sprite):
+
+    def __init__(self, filename, scale=0.4):
+
+        super().__init__(filename, scale=0.4)
+
+        self.scale = 0.4
+
+
+    def update(self):
+
+        self.change_x += self.change_x
+        self.change_y += self.change_y
+
+class Enemy(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling):
+
+        super().__init__(filename, sprite_scaling)
+
+        self.change_x += self.change_x
+        self.change_y += self.change_y
+
+
+    def update(self):
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # If we are out-of-bounds, then 'bounce'
+        if self.left < 40:
+            self.change_x *= -1
+
+        if self.right > 1480:
+            self.change_x *= -1
+
+        if self.bottom < 64:
+            self.change_y *= -1
+
+        if self.top > 1920:
+            self.change_y *= -1
 
 
 class MyGame(arcade.Window):
@@ -46,7 +88,8 @@ class MyGame(arcade.Window):
         # Set up the player
         self.player_sprite = None
 
-        self.enemy_sprite = None
+        self.change_x = 0
+        self.change_y = 0
 
         # Physics engine so we don't run into walls.
         self.physics_engine = None
@@ -75,9 +118,11 @@ class MyGame(arcade.Window):
 
         self.lives = 5
 
+        # https://api.arcade.academy/en/latest/resources.html
+
         # Set up the player
-        self.player_sprite = arcade.Sprite("slimeBlue_move.png",
-                                           scale=0.2)
+        self.player_sprite = Player("slimeBlue_move.png",
+                                           scale=0.4)
 
         self.player_sprite.center_x = 256
         self.player_sprite.center_y = 512
@@ -86,11 +131,15 @@ class MyGame(arcade.Window):
         for x in range(64, 1480, 40):
             for y in range(0, 1930, 175):
 
-                if random.randrange(30) > 9:
+                # https://api.arcade.academy/en/latest/resources.html
+
+                if random.randrange(30) > 14:
                     wall = arcade.Sprite("treeGreen_large.png", SPRITE_SCALING)
                     wall.center_x = x
                     wall.center_y = y
                     self.wall_list.append(wall)
+
+        # https://api.arcade.academy/en/latest/resources.html
 
         for x in range(0, 1550, 64):
             wall = arcade.Sprite("water.png", SPRITE_SCALING)
@@ -119,6 +168,8 @@ class MyGame(arcade.Window):
 
         for i in range(HEALTH_COUNT):
 
+            # https://api.arcade.academy/en/latest/resources.html
+
             health = arcade.Sprite("tankGreen_barrel3.png", SPRITE_SCALING_HEALTH)
 
             health_placed_successfully = False
@@ -144,7 +195,9 @@ class MyGame(arcade.Window):
 
         for i in range(ENEMY_COUNT):
 
-            enemy = arcade.Sprite("slimeBlock.png", SPRITE_SCALING_HEALTH)
+            # https://api.arcade.academy/en/latest/resources.html
+
+            enemy = Enemy("slimeBlock.png", SPRITE_SCALING)
 
             enemy_placed_successfully = False
 
@@ -153,6 +206,7 @@ class MyGame(arcade.Window):
                 # Position the coin
                 enemy.center_x = random.randrange(1420)
                 enemy.center_y = random.randrange(1920)
+                enemy.change_x = random.randrange(1, 9)
 
                 # See if the coin is hitting a wall
                 wall_hit_list = arcade.check_for_collision_with_list(enemy, self.wall_list)
@@ -193,20 +247,26 @@ class MyGame(arcade.Window):
 
         arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.BLACK, 14)
 
-        if len(self.health_list) == 0 or self.lives == 0:
+        arcade.draw_text(f"Lives: {self.lives}", 10, 50, arcade.color.BLACK, 14)
+
+        if self.lives == 0:
             arcade.draw_text("GAME OVER", 200, 300, arcade.color.BLACK, 50)
+
+        if len(self.health_list) == 0:
+            arcade.draw_text("You Win!", 290, 300, arcade.color.AVOCADO, 50)
+
 
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
-        if key == arcade.key.UP:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        if len(self.health_list) <= HEALTH_COUNT and self.lives > 0:
+            if key == arcade.key.UP:
+                self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+            elif key == arcade.key.DOWN:
+                self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+            elif key == arcade.key.LEFT:
+                self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            elif key == arcade.key.RIGHT:
+                self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
@@ -241,8 +301,7 @@ class MyGame(arcade.Window):
                 self.lives -= 1
                 arcade.play_sound(self.bad_sound)
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
+
         self.physics_engine.update()
 
         # Scroll the screen to the player
